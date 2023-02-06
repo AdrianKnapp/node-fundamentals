e89456bc039ea080e8c41fe3f0ae413927a8b20e
AdrianKnapp <adrknapp@gmail.com>
2023-02-06 08:21:14 -0300

    Using fetch request with streams

---
+++ streams/stream-http-server.js
@@ -0,0 +1,18 @@
+import http from 'node:http';
+import { Transform } from 'node:stream';
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const number = +chunk.toString() * -1;
+
+    console.log(number)
+
+    callback(null, Buffer.from(String(number)));
+  }
+}
+
+const server = http.createServer((req, res) => {
+  return req.pipe(new InverseNumberStream()).pipe(res);
+});
+
+server.listen(3334);
